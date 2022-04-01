b36dd045433c0fd1c7c2dc93ee2faf64ea51e645
Biringa Chidera <biringachidera@gmail.com>
2022-04-01 08:50:02 -0400

    added ..

--- concrete1.py
+++ concrete1.py
@@ -1,5 +1,8 @@
 from PyQt6.QtWidgets import *
 from observer import Observer
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
 import pyqtgraph
 
 
@@ -8,7 +11,10 @@ class Concrete1(Observer):
         super().__init__(subject)
 
         # add gui code here, use self.subject for data
+    
+    def scatter_plot(self, data):
+        sns.scatterplot(x = data[0], y = data[1])
 
     def process_update(self):
         # add refresh code here, use self.subject for data
-        pass
+        return self.scatter_plot(self.subject)
